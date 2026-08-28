9d61c077ac7d1be30f096470a624f6e9003b9f24
Izabela K <aizeilla12344@gmail.com>
2026-08-28 23:40:17 +0200

    feat(domain): update User with Event references

--- src/main/java/com/vixello/tickets/domain/User.java
+++ src/main/java/com/vixello/tickets/domain/User.java
@@ -1,14 +1,13 @@
 package com.vixello.tickets.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -31,6 +30,23 @@ public class User {
     private String email;
 
     //TODO: organized, attending, staffing events
+
+    @OneToMany(mappedBy = "organizer", cascade = CascadeType.ALL)
+    private List<Event> organizedEvents = new ArrayList<>();
+
+    @ManyToMany()
+    @JoinTable(name = "user_attending_events",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> attendingEvents = new ArrayList<>();
+
+    @ManyToMany()
+    @JoinTable(name = "user_staffing_events",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id")
+    )
+    private List<Event> staffingEvents = new ArrayList<>();
 
     @CreatedDate
     @Column(name = "createdAt", updatable = false, nullable = false)
